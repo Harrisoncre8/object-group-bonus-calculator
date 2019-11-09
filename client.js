6c75d96992a1ea4e6aba81b7da3bf3e7277828ae
Harrison Nguyen <nguy2173@umn.edu>
2019-11-09 11:23:12 -0600

    Changes to client

--- client.js
+++ client.js
@@ -40,33 +40,59 @@ const employees = [
 // This is not a race. Everyone on your team should understand what is happening.
 // Ask questions when you don't.
 
-console.log( employees );
+// Loop over the `employees` array and do the following:
+// * use each employee object as the input to the function described below.
+// * `console.log` the results of each iteration
+for ( let i=0; i<employees.length; i++ ){
+} 
+}// end for loop
+  
+// function that takes in employees object and returns new object
+function newObjectEmployee (employeesObject){
+}// end newObjectEmployee function
 
 
-// Master Function - calculates & changes object
-function employeeSalary(){
-  // run calculate bonus function
-  // return employee object with new properties
-}// end function
+//  function calculates & changes object
+// function employeeSalary(){
+//   // run calculate bonus function
+//   calculateBonus();
+//   // return employee object with new properties
+//   let newObjectEmployee = {
+//     name: newObjectName,
+//     employeeNumber: newObjectEmployeeNumber,
+//     inputAnnualSalary: newSalary,
+//     reviewRating: newObjectRating
+//   }// end new object
+//   return newObjectEmployee;
+// }// end employeeSalary function
 
-
-// Bonus calculation function
+// // Bonus calculation function
 function calculateBonus(person){
+for ( i = 0; i < employees.length; i++ ){
 // if a rating of a 2 or below - does not receive a bonus
-if ( employees.annualSalary <= 2 ){
+if ( employees.reviewRating <= 2 ){
   return 'no bonus';
 } // end rating of <= 2 statement
 // a rating of a 3 - receive a base bonus of 4% of their base annual income
-if ( employees.annualSalary === 3 ){
-{ employees.annualSalary * .04 } + employees.annualSalary;
+else if ( employees.reviewRating === 3 ){
+  return (employees.annualSalary * .04)  + parseInt(employees.annualSalary);
 } // end 3 rating add 4% to salary
 // a rating of a 4 should receive a base bonus of 6% of their base annual income
-if (  )
+else if ( employees.reviewRating === 4 ){
+  return ( employees.annualSalary * .06 ) + parseInt(employees.annualSalary);
+}// end 4 rating
 // a rating of a 5 should receive a base bonus of 10% of their base annual income
-  
-// if index of employee number is === 5 , and receive an additional 5%
-//  && if their annual income is greater than $65,000, they should have their bonus adjusted down 1%
-
-// No bonus can be above 13% or below 0% total
+else if (employees.reviewRating === 5 ){
+  return ( employees.annualSalary * .10 ) + parseInt(employees.annualSalary);
+} // end 5 rating 
+// if index of employee number is === 5 , receive an additional 5%
+// else if (employees.employeeNumber[i] === 5){
+//   return ( employees.annualSalary * .05 ) + parseInt(employees.annualSalary)
+//}// end additional 5%
+}// end for loop
 }// end bonus calculation function
 
+
+// // No bonus can be above 13% or below 0% total
+// // &&if their annual income is greater than $65,000, they should have their bonus adjusted down 1%
+// console.log(calculateBonus(employees));
