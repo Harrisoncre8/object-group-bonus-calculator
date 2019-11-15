ff9ecdcc4737c5c51668343ff933c9f40db96c60
Harrison Nguyen <nguy2173@umn.edu>
2019-11-15 08:41:46 -0600

    Loop through array and calculate bonus

--- client.js
+++ client.js
@@ -40,83 +40,39 @@ const employees = [
 // This is not a race. Everyone on your team should understand what is happening.
 // Ask questions when you don't.
 
-let newEmployeesArray = [];
 
-function addEmployee( employees ){
-  // loop through employees array
-  for (let i=0; i<employees.length; i++){
-    let newEmployees = {
-      name: name,
-      bonusPercentage: bonusPercentage,
+function createNewEmployees(employee) {
+  for (let i = 0; i < employees.length; i++) {
+    console.log(employees[i]);
+    let bounusPercentage = calculateBonus(employee);
+    let totalBonus = bounusPercentage * employee.annualSalary;
+    let totalCompensation = Number(employee.annualSalary) + totalBonus;
+
+    let newEmployee = {
+      name: employee.name,
+      bounusPercentage: bounusPercentage,
       totalCompensation: totalCompensation,
       totalBonus: totalBonus
-    } // end object literal
-    calculateBonus();
-    newEmployeesArray.push();
+    }// end object
+    return newEmployee;
   }// end for loop
-}// end addEmployee function
-
-function calculateBonus(){
-  newEmployees.bonusPercentage = 0;
-  if (employees.employeeNumber[i] === 3 && employees.reviewRating === 5 && presInt(employees.annualSalary) > 65000){
-    newEmployees.bonusPercentage = 0.12;
-    newEmployees.totalBonus = newEmployees.bonusPercentage * employees.annualSalary;
-    return newEmployees.totalCompensation = newEmployees.totalBonus + paresInt(employees.annualSalary);
-  }
-  if (employees.employeeNumber[i] === 3 && employees.reviewRating === 4 && presInt(employees.annualSalary) > 65000){
-    newEmployees.bonusPercentage = 0.09;
-    newEmployees.totalBonus = newEmployees.bonusPercentage * employees.annualSalary;
-    return newEmployees.totalCompensation = newEmployees.totalBonus + paresInt(employees.annualSalary);
-  }
-  if (employees.employeeNumber[i] === 3 && employees.reviewRating === 3 && presInt(employees.annualSalary) > 65000){
-    newEmployees.bonusPercentage = 0.08;
-    return newEmployees.totalCompensation = newEmployees.totalBonus + paresInt(employees.annualSalary);
-  }
-  if (employees.employeeNumber[i] === 3 && employees.reviewRating <= 2 && presInt(employees.annualSalary) > 65000){
-    newEmployees.bonusPercentage = 0.04;
-    newEmployees.totalBonus = newEmployees.bonusPercentage * employees.annualSalary;
-    return newEmployees.totalCompensation = newEmployees.totalBonus + paresInt(employees.annualSalary);
-  }
-  if (employees.employeeNumber[i] === 3 && employees.reviewRating === 5){
-    newEmployees.bonusPercentage = 0.13;
-    newEmployees.totalBonus = newEmployees.bonusPercentage * employees.annualSalary;
-    return newEmployees.totalCompensation = newEmployees.totalBonus + paresInt(employees.annualSalary);
-  }
-  if (employees.employeeNumber[i] === 3 && employees.reviewRating === 4){
-    newEmployees.bonusPercentage = 0.10;
-    newEmployees.totalBonus = newEmployees.bonusPercentage * employees.annualSalary;
-    return newEmployees.totalCompensation = newEmployees.totalBonus + paresInt(employees.annualSalary);
-  }
-  if (employees.employeeNumber[i] === 3 && employees.reviewRating === 3){
-    newEmployees.bonusPercentage = 0.09;
-    return newEmployees.totalCompensation = newEmployees.totalBonus + paresInt(employees.annualSalary);
-  }
-  if (employees.employeeNumber[i] === 3 && employees.reviewRating <= 2){
-    newEmployees.bonusPercentage = 0.05;
-    newEmployees.totalBonus = newEmployees.bonusPercentage * employees.annualSalary;
-    return newEmployees.totalCompensation = newEmployees.totalBonus + paresInt(employees.annualSalary);
-  }
-  if (employees.reviewRating <= 2){
-    newEmployees.bonusPercentage = 0;
-    newEmployees.totalBonus = newEmployees.bonusPercentage * employees.annualSalary;
-    return newEmployees.totalCompensation = employees.annualSalary;
-  }
-  if (employees.reviewRating === 3){
-    newEmployees.bonusPercentage = 0.04;
-    newEmployees.totalBonus = newEmployees.bonusPercentage * employees.annualSalary;
-    return newEmployees.totalCompensation = newEmployees.totalBonus + paresInt(employees.annualSalary);
-  }
-  if ( employees.reviewRating === 4){
-    newEmployees.bonusPercentage = 0.06;
-    newEmployees.totalBonus = newEmployees.bonusPercentage * employees.annualSalary;
-    return newEmployees.totalCompensation = newEmployees.totalBonus + parseInt(employees.annualSalary);
-  }
-  if (employees.reviewRating === 5){
-    newEmployees.bonusPercentage = 0.10;
-    newEmployees.totalBonus = newEmployees.bonusPercentage * employees.annualSalary;
-    return newEmployees.totalCompensation = newEmployees.totalBonus + parseInt(employees.annualSalary);
-  }
-}// end calculateBonus function
+} // end createEmployees
 
 console.log( employees );
+console.log( createNewEmployees( employees[0] ) );
+
+function calculateBonus(employee) {
+  let bounusPercentage = 0;
+  if (employee.reviewRating <= 2){
+     bounusPercentage = 0;
+} else if (employee.reviewRating = 3){
+     bounusPercentage = .04;
+} else if (employee.reviewRating = 4){
+   bounusPercentage = .06;
+} else if (employee.reviewRating = 5){
+   bounusPercentage = .10;
+}
+return bounusPercentage;
+}// end calculateBonus
+
 
